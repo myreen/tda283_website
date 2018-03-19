913217d8c96b3ff67e83261b1f190018559aff99
Alex Gerdes <agerdes@me.com>
2018-03-19 15:37:09 +0100

    Deploy to 2017 catalog.

--- deploy_website.sh
+++ deploy_website.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 runghc website.hs build 
-rsync -avzh --exclude '*.swp' _site/* alexg@remote12.chalmers.se:/chalmers/groups/edu2009/www/www.cse.chalmers.se/course/TDA283_Compiler_Construction/
+rsync -avzh --exclude '*.swp' _site/* alexg@remote12.chalmers.se:/chalmers/groups/edu2009/www/www.cse.chalmers.se/course.2017/TDA283_Compiler_Construction/
